c0e0df798f715b8c0191571afbdc760a953001fb
nnawar706 <nnawar706@gmail.com>
2023-06-17 11:46:43 +0600

    multiple-image-request

--- app/Models/GeneralSetting.php
+++ app/Models/GeneralSetting.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class GeneralSetting extends Model
 {
     use HasFactory;
+
+    protected $table = 'general_settings';
+
+    protected $fillable = [
+        'notification_on_new_order',
+        'mail_on_new_signin',
+        'promo_on_signin'
+    ];
+
+    protected $hidden = [
+        'created_at'
+    ];
 }
