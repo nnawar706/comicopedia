036a2ae02a0301802eaa8534dc232b54495df3a3
nnawar706 <nnawar706@gmail.com>
2023-06-17 11:48:52 +0600

    multiple-image-request

--- app/Models/SiteInformation.php
+++ app/Models/SiteInformation.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class SiteInformation extends Model
 {
     use HasFactory;
+
+    protected $table = 'site_information';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'contact',
+        'logo_path',
+        'favicon_path',
+        'about',
+        'facebook_url',
+        'instagram_url'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
 }
