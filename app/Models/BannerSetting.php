6c074b8a81b14f8f2135419555f108bfeb623ebd
nnawar706 <nnawar706@gmail.com>
2023-06-17 11:44:28 +0600

    multiple-image-request

--- app/Models/BannerSetting.php
+++ app/Models/BannerSetting.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class BannerSetting extends Model
 {
     use HasFactory;
+
+    protected $table = 'banner_settings';
+
+    protected $fillable = [
+        'type_id',
+        'photo_path'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function type()
+    {
+        return $this->belongsTo(BannerType::class, 'type_id');
+    }
 }
--- app/Models/BannerType.php
+++ app/Models/BannerType.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class BannerType extends Model
 {
     use HasFactory;
+
+    protected $table = 'banner_type';
+
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $hidden = [
+        'created_at'
+    ];
+
+    public function banners()
+    {
+        return $this->hasMany(BannerType::class, 'type_id');
+    }
 }
