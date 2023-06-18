8fe325601d3de49cc118bfb984c19c79dac40f8d
nnawar706 <nnawar706@gmail.com>
2023-06-18 17:50:32 +0600

    site-info

---
+++ database/migrations/2023_06_18_114542_add_fields_to_general_setting.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('general_settings', function (Blueprint $table) {
+            $table->tinyInteger('welcome_mail_on_new_user_sign_in')->nullable()->after('promo_on_new_user_sign_in');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('general_setting', function (Blueprint $table) {
+            //
+        });
+    }
+};
