098cff6986218659bb05e792fe37b89b3ff9dd65
nnawar706 <nnawar706@gmail.com>
2023-06-21 18:18:57 +0600

    pie-chart-done

---
+++ database/migrations/2023_06_21_121112_add_color_to_genre.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('genre', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('genre', function (Blueprint $table) {
+            $table->string('bg_color_code')->after('order')->default('#989');
+            $table->string('bg_hover_color_code')->after('bg_color_code')->default('#765');
+        });
+    }
+};
