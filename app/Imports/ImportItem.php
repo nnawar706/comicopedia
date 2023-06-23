f34155af05a4c2d292cbfe0b1360a0da5f790f12
nnawar706 <nnawar706@gmail.com>
2023-06-23 13:17:36 +0600

    composer update

---
+++ app/Imports/ImportItem.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Item;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ImportItem implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Item([
+            //
+        ]);
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            redirect()->back()->with('message', $validator->errors()->first())
+        );
+    }
+}
