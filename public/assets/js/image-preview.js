7d85271c2071076dbbd06b631eec3efe6b1e8620
nnawar706 <nnawar706@gmail.com>
2023-06-24 16:04:16 +0600

    banner-view

--- public/assets/js/image-preview.js
+++ public/assets/js/image-preview.js
@@ -101,3 +101,55 @@ $(document).on("change", ".file-input", function () {
         alert("This browser does not support HTML5 FileReader.");
     }
 });
+
+$(document).on("change", ".file-input", function () {
+    var filesCount = $(this)[0].files.length;
+
+    var textbox = $(this).prev();
+
+    filesCount===1 ? textbox.text('1 file selected') : textbox.text(filesCount + ' files selected');
+
+    if (typeof FileReader != "undefined") {
+        var dvPreview = $("#newsPreview");
+        dvPreview.html("");
+        $($(this)[0].files).each(function () {
+            var file = $(this);
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var img = $("<img />");
+                img.attr("style", "width: 150px; height:100px; padding: 10px");
+                img.attr("src", e.target.result);
+                dvPreview.append(img);
+            };
+            reader.readAsDataURL(file[0]);
+        });
+    } else {
+        alert("This browser does not support HTML5 FileReader.");
+    }
+});
+
+$(document).on("change", ".file-input", function () {
+    var filesCount = $(this)[0].files.length;
+
+    var textbox = $(this).prev();
+
+    filesCount===1 ? textbox.text('1 file selected') : textbox.text(filesCount + ' files selected');
+
+    if (typeof FileReader != "undefined") {
+        var dvPreview = $("#salePreview");
+        dvPreview.html("");
+        $($(this)[0].files).each(function () {
+            var file = $(this);
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var img = $("<img />");
+                img.attr("style", "width: 150px; height:100px; padding: 10px");
+                img.attr("src", e.target.result);
+                dvPreview.append(img);
+            };
+            reader.readAsDataURL(file[0]);
+        });
+    } else {
+        alert("This browser does not support HTML5 FileReader.");
+    }
+});
---
+++ resources/views/admin/includes/banners/newsletter.blade.php
@@ -0,0 +1,50 @@
+<h2 class="accordion-header" id="flush-headingOne">
+    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+        Newsletter Us <span style="margin-left:10px;font-size: 12px; color: #5a5c69">*Dimensions: 336x280</span>
+    </button>
+</h2>
+<div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+    <div class="accordion-body">
+        <div class="card card-body">
+            <div class="d-flex flex-nowrap">
+                @if(count($data[2]['banners']) === 0)
+                    <p>No Banner Found</p>
+                @else
+                    @foreach($data[2]['banners'] as $item)
+                        <div style="position: relative; display: inline-block">
+                            <img class="img-thumbnail" src="{{ asset($item['photo_path']) }}" height="100" width="100">
+                            <a href="/admin/banners/delete/{{ $item['id'] }}"><button style="position: absolute; top:15px; right:15px; padding:0; background-color: transparent; border:none; color: #fff; font-size: 20px; cursor:pointer">&times;</button></a>
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+            <button type="button" class="btn btn-primary text-center" data-bs-toggle="modal"
+                    data-bs-target="#updateNews" data-whatever="@mdo">
+                Add New
+            </button>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="updateNews" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title" id="exampleModalLabel1">Newsletter</h6>
+            </div>
+            <div class="modal-body image-body text-center">
+                <form action="/admin/banners/3" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="file-drop-area">
+                        <span class="choose-file-button">Choose Files</span>
+                        <span class="file-message">or drag and drop files here</span>
+                        <input type="file" name="images[]" class="file-input" accept=".jpg,.jpeg,.png" required multiple>
+                    </div>
+                    <div id="newsPreview"></div>
+                    <hr>
+                    <button type="submit" class="btn btn-primary">Add</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
