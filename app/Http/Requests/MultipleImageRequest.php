5d6641f152493711c85748fd17c3051a98e2f38a
nnawar706 <nnawar706@gmail.com>
2023-06-24 14:55:50 +0600

    banner-delete-done

--- app/Http/Requests/MultipleImageRequest.php
+++ app/Http/Requests/MultipleImageRequest.php
@@ -25,17 +25,58 @@ class MultipleImageRequest extends FormRequest
      */
     public function rules()
     {
-        return [
+        $type = $this->route('id');
+
+        $rules = [
             'images' => 'required|array',
             'images.*' => 'required|image|mimes:jpg,png,jpeg,svg,gif|max:2048'
+        ];
+
+        if($type == 1)
+        {
+            $rules['images.*'] = 'dimensions:width=970,height=250';
+        }
+
+        else if($type == 2)
+        {
+            $rules['images.*'] = 'dimensions:width=336,height=280';
+        }
+
+        else if($type == 3)
+        {
+            $rules['images.*'] = 'dimensions:width=970,height=90';
+        }
+
+        else if($type == 4)
+        {
+            $rules['images.*'] = 'dimensions:width=120,height=240';
+        }
+
+        else if($type == 5)
+        {
+            $rules['images.*'] = 'dimensions:width=120,height=240';
+        }
+
+        else if($type == 6)
+        {
+            $rules['images.*'] = 'dimensions:width=160,height=600';
+        }
+
+        return $rules;
+    }
+
+    public function messages()
+    {
+        return [
+            'images.*.dimensions' => 'Dimensions of the selected images are not valid.'
         ];
     }
 
     protected function failedValidation(Validator $validator)
     {
         throw new HttpResponseException(
-//            redirect()->back()->with('message', $validator->errors()->first())
-            response()->json([$validator->errors()->all()])
+            redirect()->back()->with('message', $validator->errors()->first())
+//            response()->json([$validator->errors()->all()])
         );
     }
 }
--- resources/views/admin/includes/general/sidebar.blade.php
+++ resources/views/admin/includes/general/sidebar.blade.php
@@ -1,9 +1,9 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('admin-dashboard') }}">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <img src="{{ asset('uploads/general/logo.png') }}" height="30" width="30">
-                </div>
+{{--                <div class="sidebar-brand-icon rotate-n-15">--}}
+{{--                    <img src="{{ asset('uploads/general/logo.png') }}" height="30" width="30">--}}
+{{--                </div>--}}
                 <div class="sidebar-brand-text mx-3">Mangamania</div>
             </a>
 
