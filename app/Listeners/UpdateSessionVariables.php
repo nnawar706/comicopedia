99ae0b70ddb123647a06b4f38615d1951a086682
nnawar706 <nnawar706@gmail.com>
2023-07-05 15:21:20 +0600

    session-variables

---
+++ app/Listeners/UpdateSessionVariables.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\Cart;
+use App\Services\CartService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Session;
+
+class UpdateSessionVariables
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(Login $event)
+    {
+        $user = $event->user;
+
+        Cart::where('session_id', Session::get('customer_unique_id'))->update([
+            'user_id' => $user->id,
+        ]);
+
+        Session::put('cart_quantity', (new CartService(new Cart()))->getItemValue());
+
+        Session::put('cart_price', (new CartService(new Cart()))->getAuthCartAmount());
+    }
+}
