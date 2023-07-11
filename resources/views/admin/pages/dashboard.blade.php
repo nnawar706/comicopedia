ad290aa0434b56037d1c3456c258f6f9a769a2e3
nnawar706 <nnawar706@gmail.com>
2023-07-11 15:09:59 +0600

    order-map

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -36,7 +36,6 @@
                 </div>
             </div>
             <div class="col-lg-6 mb-4">
-                <!-- Not Decided Yet -->
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-primary">Order Locations</h6>
