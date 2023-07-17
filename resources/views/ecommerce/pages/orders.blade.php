a3198a10343307498dec942f788fbfec0c9198d2
nnawar706 <nnawar706@gmail.com>
2023-07-17 10:22:10 +0600

    order

--- resources/views/ecommerce/pages/orders.blade.php
+++ resources/views/ecommerce/pages/orders.blade.php
@@ -9,8 +9,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <table class="table">
-                        <thead>
+                    @if(count($data) === 0)
+                        <p style="text-align: center">No Order Found.</p>
+                    @else
+                        <table class="table">
+                            <thead>
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Order Number</th>
@@ -20,21 +23,26 @@
                                 <th scope="col">Status</th>
                                 <th scope="col"></th>
                             </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($data as $key => $item)
-                        <tr>
-                            <th scope="row">{{ $key+1 }}</th>
-                            <td>{{ $item['order_no'] }}</td>
-                            <td>{{ $item['delivery_tracking_no'] }}</td>
-                            <td>{{ $item['items_count'] }}</td>
-                            <td>&#2547; {{ $item['total']+$item['shipping_cost']-$item['promo_discount'] }}</td>
-                            <td>{{ $item['status']['name'] }}</td>
-                            <td><a href="{{ route('order-detail-pdf', ['id' => $item['id']]) }}"><i class="fa fa-file-pdf" style="color:#1c1c1c;"></i></a></td>
-                        </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @foreach($data as $key => $item)
+                                <tr @if($item['status']['name']=='Cancelled')
+                                        class="table-danger"
+                                    @elseif($item['status']['name']=='Delivered')
+                                        class="table-success"
+                                    @endif>
+                                    <th scope="row">{{ $key+1 }}</th>
+                                    <td>{{ $item['order_no'] }}</td>
+                                    <td>{{ $item['delivery_tracking_no'] }}</td>
+                                    <td>{{ $item['items_count'] }}</td>
+                                    <td>&#2547; {{ $item['total']+$item['shipping_cost']-$item['promo_discount'] }}</td>
+                                    <td>{{ $item['status']['name'] }}</td>
+                                    <td><a href="{{ route('order-detail-pdf', ['id' => $item['id']]) }}"><i class="fa fa-file-pdf" style="color:#1c1c1c;"></i></a></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
