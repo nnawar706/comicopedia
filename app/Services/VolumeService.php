9cda1dab53de4900d678dd05935c41719e69873f
nnawar706 <nnawar706@gmail.com>
2023-07-22 10:24:42 +0600

    order

--- app/Services/VolumeService.php
+++ app/Services/VolumeService.php
@@ -254,6 +254,27 @@ class VolumeService
             ->selectRaw("count(*) as total, monthname(created_at) as month_name, month(created_at) as month")
             ->groupByRaw("month_name, month")->get();
 
+        $data = json_decode($data, true);
+
+        $curMonth = date('n');
+
+        for($i=0;$i<12;$i++) {
+            $month = ($curMonth - $i + 12) % 12;
+            $month = $month === 0 ? 12 : $month;
+
+            $exist = count(array_filter($data, function ($obj) use ($month) {
+                    return $obj['month'] == $month;
+                })) > 0;
+
+            if(!$exist) {
+                $data[] = array(
+                    'total'      => 0,
+                    'month_name' => date('F', mktime(0, 0, 0, $month, 1)),
+                    'month'      => $month
+                );
+            }
+        }
+
         return $data;
     }
 }
