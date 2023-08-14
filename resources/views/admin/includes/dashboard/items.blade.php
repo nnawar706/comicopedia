66654d5da9a80ce775a2439e1951687637ce5719
nnawar706 <nnawar706@gmail.com>
2023-08-14 21:36:28 +0600

    cart-to-order-ratio

--- resources/views/admin/includes/dashboard/items.blade.php
+++ resources/views/admin/includes/dashboard/items.blade.php
@@ -3,7 +3,7 @@
         <!-- Comic Card -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Most Viewed Volumes {{ $data['cart_to_order_rate'] }}</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Most Viewed Volumes</h6>
             </div>
             <div class="card-body">
                 @php
--- resources/views/admin/includes/dashboard/reports.blade.php
+++ resources/views/admin/includes/dashboard/reports.blade.php
@@ -34,27 +34,22 @@
         </div>
     </div>
     <!-- Earnings (Monthly) Card Example -->
-    @php
-    $total = 0;
-
-    foreach($data['order_data'] as $item)
-        $total += $item['orders_count'];
-    @endphp
+    
     <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-info shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Completed Orders
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Cart to Order Ratio
                         </div>
                         <div class="row no-gutters align-items-center">
                             <div class="col-auto">
-                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ ($data['order_data'][3]['orders_count']/$total) * 100 }}%</div>
+                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $data['cart_to_order_rate']['cart_to_order_ratio'] * 100 }}%</div>
                             </div>
                             <div class="col">
                                 <div class="progress progress-sm mr-2">
                                     <div class="progress-bar bg-info" role="progressbar"
-                                        style="width: {{ ($data['order_data'][3]['orders_count']/$total) * 100 }}%" aria-valuenow="{{ ($data['order_data'][3]['orders_count']/$total) * 100 }}" aria-valuemin="0"
+                                        style="width: {{ $data['cart_to_order_rate']['cart_to_order_ratio'] * 100 }}%" aria-valuenow="{{ $data['cart_to_order_rate']['cart_to_order_ratio'] * 100 }}" aria-valuemin="0"
                                         aria-valuemax="100"></div>
                                 </div>
                             </div>
